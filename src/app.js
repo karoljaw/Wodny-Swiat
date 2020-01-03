9139d25ab55a538d6cb5f514c976896b1b7ab933
Karol Jaworek <karoljaworek04@gmail.com>
2020-01-03 13:53:59 +0100

    info section

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import '@fortawesome/fontawesome-free/js/all';
-// import Icon from './logo.png';
+
 import './style.css';
 
 console.log("hello World")
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const postCSSPlugins = [
   require('postcss-simple-vars'),
@@ -10,8 +12,12 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.[contentHash].js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: "./src/templete.html"
+  })],
   module: {
     rules: [
       {
@@ -19,17 +25,26 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader',
-          { loader: 'postcss-loader', options: { plugins: postCSSPlugins } }
+          { loader: 'postcss-loader', options: { plugins: postCSSPlugins }}
         ],
       },
-      // {
-      //   test: /\.(png|svg|jpg|gif)$/,
-      //   use: [
-      //     'file-loader',
-      //   ],
-      // },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+            esModule: false,
+          }
+        }
+      },
     ],
   },
-  mode: "development",
+  // mode: "development",
   watch: true
 };
